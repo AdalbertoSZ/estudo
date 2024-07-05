44680158cfc3400dd6b379f7b7be56f51a5aac5f
AdalbertoSZ <adalberto.fastcomp@gmail.com>
2024-07-05 13:43:47 -0300

    erro

--- site/conteudo/sair.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION['usuario']);
-    unset($_SESSION['senha']);
-    echo "<script> location.href='../index.html';</script>";
-    //header("Location: login.php");
-?>
