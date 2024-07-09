16580eb3094ad45ae02ab750a952848b153227b7
AdalbertoSZ <160979875+AdalbertoSZ@users.noreply.github.com>
2024-07-08 22:41:50 -0300

    codando

--- Novo site/arquivos/trpatrimonio.php
+++ Novo site/arquivos/trpatrimonio.php
@@ -11,10 +11,31 @@
         $patrimonio = $resultado['Patrimonio'];
         $modelo = $resultado['Modelo'];
         $serial = $resultado['Nserie'];
-        //print_r($resultado['Patrimonio']);
-        //if(mysqli_num_rows($encontrou) < 1){
-		//	header('Location: patrimonio.php');
-    	//}
+        
+        if (isset($_POST['GravaTR'])){
+            //gravando!!
+            $patrimonio = $_SESSION['patrimonio'];
+            $modelo = $_SESSION['modelo'];
+            $Cliente = $_POST['Lcliente'];
+            $Data = $_POST['Data'];
+			$Anotacao = $_POST['Anotacao'];
+			$buscaidcli = "SELECT * FROM Clientes WHERE Cliente = '$Cliente' ";
+			$encidcli = $banco->query($buscaidcli);
+			$dadosidcli = mysqli_fetch_assoc($encidcli);
+			$idcli = $dadosidcli['Idcliente'];
+
+            $gravadados = mysqli_query ($banco, "INSERT INTO VLocalizacao (Patrimonio,Modelo,Datatrs,Cliente,Anotacao) 
+				VALUES ('$patrimonio','$modelo','$Data','$idcli','$Anotacao')") ;
+				header('Location: patrimonio.php');
+        }
+        $listacli = "";
+        $buscacli = "SELECT * FROM Clientes ORDER BY Cliente";
+        $achoucli = mysqli_query($banco,$buscacli);
+        while ($lclie = mysqli_fetch_assoc($achoucli)) { 
+            $cli = $lclie['Cliente'];
+            $listacli .= "<option value='" . $cli . "'></option>";	
+        }
+        
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -42,49 +63,52 @@
     <main class="conteudo">
     <?php
         if ( $numpatr == 1 ) {
-            echo "<p>Achei 1 patrimonio</p>";
+            //echo "<p>Achei 1 patrimonio</p>";
+            $_SESSION['patrimonio'] = $patrimonio;
+            $_SESSION['modelo'] = $modelo;
             echo "<form action='trpatrimonio.php' method='post'>
                 <table class='nwpatr'>
                     <tr>
                         <td>Patrimonio</td>
-                        <td>" .$patrimonio. "</td>
+                        <td><h4>" .$patrimonio. "</h4></td>
                     </tr>
                     <tr>
                         <td>Modelo</td>
-                        <td>" .$modelo. "</td>
+                        <td><h4>" .$modelo. "</h4></td>
                     </tr>
                     <tr>
                         <td>Serial</td>
-                        <td>" .$serial. "</td>
+                        <td><h4>" .$serial. "</h4></td>
                     </tr>
                     <tr>
                         <td>Cliente</td>
-                        <td>
+                        <td><input class='entra' type='text' name='Lcliente' list='lcli' value= '' required>
+            			<datalist id='lcli'>";
+                        echo $listacli;
+                        echo "</datalist>
                         </td>
                     </tr>
                     <tr>
+                        <td>Data</td>
+                        <td> <input class='entra' type='date' name='Data' required> </td>
+                    </tr>
+                    <tr>
                         <td>Anotação</td>
-                        <td><input type='text' name='anotacao' placeholder='Anotação' required></td>
+                        <td><input type='text' name='Anotacao' placeholder='Anotação' required></td>
+                    </tr>
+                    <tr>
+                        <td><button><a href='patrimonio.php'>Cancelar</a></button></td>
+                        <td class='rodape'><input class='inputsubmit' type='submit' name='GravaTR' value='Gravar'></td>
                     </tr>
                 </table>
 
             </form>";
+            
         } else {
             echo "<p>Não achei o patrimonio</p>";
+            //echo $listacli;
         }
     ?>
-                    <input class='entra' type='text' name='Lcliente' list='lcli' value= '' required>
-            			<datalist id='lcli'>
-            				<?php $buscacli = "SELECT * FROM Clientes ORDER BY Cliente";
-            				$achoucli = mysqli_query($banco,$buscacli);
-            				while ($lclie = mysqli_fetch_assoc($achoucli)) { ?>
-            				<option value="<?php echo $lclie['Cliente']; ?>" ><?php echo $lclie['Cliente']; ?> 
-            				</option> <?php 	
-            				}
-            		 		?>
-            			
-            			</datalist>
-     <!-- <td><input type='text' name='anotacao' placeholder='Anotação' required></td>        -->
     </main>
 </body>
 </html>
